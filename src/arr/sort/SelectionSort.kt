b9439f66c82a698e8704ecd6aec339584cdfb8dc
Yx-Z <yx-z@outlook.com>
2017-10-14 16:04:40 -0500

    selection sort

--- src/arr/sort/SelectionSort.kt
+++ src/arr/sort/SelectionSort.kt
@@ -1,5 +1,17 @@
 package arr.sort
 
 fun IntArray.selectionSort() {
+	indices.forEach {
+		withIndex()
+				.filterIndexed { idx, _ -> idx > it }
+				.forEach { (idx, i) ->
+					if (this[it] > i) {
+						swap(it, idx)
+					}
+				}
+	}
+}
 
+fun main(args: Array<String>) {
+	println(testCorrectness { selectionSort() })
 }
