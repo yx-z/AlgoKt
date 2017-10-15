d2cd90cc9b885d47ec2f92919c8f410577bf2d4b
Yx-Z <yx-z@outlook.com>
2017-10-14 21:38:37 -0500

    reverse words raw

--- src/str/ReverseWords.kt
+++ src/str/ReverseWords.kt
@@ -2,11 +2,12 @@ package str
 
 fun main(args: Array<String>) {
 	val testStr = "This is just a sample test sentence with a lot of words and spaces in it"
-	println(testStr.reverseWordByBuilder())
+	println(testStr.reverseWordsByBuilder())
 	println(testStr.reverseWords())
+	println(testStr.reverseWordsRaw())
 }
 
-fun String.reverseWordByBuilder() = split(" ")
+fun String.reverseWordsByBuilder() = split(" ")
 		.joinToString(" ") { StringBuilder(it).reverse() }
 
 fun String.reverseWords() = split(" ").joinToString(" ") { it.reverse() }
@@ -17,4 +18,19 @@ fun String.reverse(): String {
 		sb.append(this[it])
 	}
 	return sb.toString()
+}
+
+fun String.reverseWordsRaw(): String {
+	val sb = StringBuilder()
+	val word = StringBuilder()
+	forEach {
+		if (it == ' ') {
+			sb.append(word.toString().reverse())
+			sb.append(' ')
+			word.setLength(0)
+		} else {
+			word.append(it)
+		}
+	}
+	return sb.append(word.toString().reverse()).toString()
 }
