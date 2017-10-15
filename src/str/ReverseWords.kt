68312552e93a40e5fe58468acfe6d647345d18ac
Yx-Z <yx-z@outlook.com>
2017-10-14 21:32:13 -0500

    reverse words duhh

--- src/str/ReverseWords.kt
+++ src/str/ReverseWords.kt
@@ -1,7 +1,20 @@
 package str
 
 fun main(args: Array<String>) {
-	println("This is just a sample test sentence with a lot of words and spaces in it".reverseWords())
+	val testStr = "This is just a sample test sentence with a lot of words and spaces in it"
+	println(testStr.reverseWordByBuilder())
+	println(testStr.reverseWords())
 }
 
-fun String.reverseWords() = split(" ").joinToString(" ") { StringBuilder(it).reverse() }
+fun String.reverseWordByBuilder() = split(" ")
+		.joinToString(" ") { StringBuilder(it).reverse() }
+
+fun String.reverseWords() = split(" ").joinToString(" ") { it.reverse() }
+
+fun String.reverse(): String {
+	val sb = StringBuilder()
+	((length - 1) downTo 0).forEach {
+		sb.append(this[it])
+	}
+	return sb.toString()
+}
