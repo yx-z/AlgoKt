134b15e29805b135ba7e35f45b1c0aa8511a1100
Yx-Z <yx-z@outlook.com>
2018-03-12 15:49:54 -0500

    construct avlt

--- src/dp/ConstructAVLT.kt
+++ src/dp/ConstructAVLT.kt
@@ -1,6 +1,8 @@
 package dp
 
-import min
+import get
+import max
+import kotlin.math.abs
 
 // similar to 'ConstructBST'
 // construct a balanced BST (AVL Tree) that must satisfy |h(left) - h(right)| < 2
@@ -16,32 +18,42 @@ fun IntArray.avltCost(): Int {
 	//            sum(this[i..j] + dp[i, r - 1].second + dp[r + 1, j].second), o/w
 	//            , where r satisfies i <= r <= j, |dp[i, r - 1].first - dp[r + 1, j].first| < 2
 	//            , and minimizes dp[i, r - 1].second + dp[r + 1, j].second
-	val dp = Array(size + 1) { Array(size + 1) { 0 to 0 } }
+	val dp = Array(size) { Array(size) { 0 to 0 } }
 	for (j in 0 until size) {
 		for (i in j downTo 0) {
-			var minR = i
 			var minCost = Int.MAX_VALUE
+			var minH = 0
 			for (r in i..j) {
-				var cost = when {
-					r - 1 < 0 -> dp[r + 1][j].second
-					r + 1 >= size -> dp[i][r - 1].second
-					else -> dp[i][r - 1].second + dp[r + 1][j].second
-				}
-				if (r - 1 >= 0 && r + 1 < size) {
-					if (Math.abs(dp[r + 1][j].first - dp[i][r - 1].first) >= 2) {
-						cost = Int.MAX_VALUE
+				var cost = this[i..j].sum()
+				var h = 0
+				when {
+					r - 1 < 0 -> {
+						if (dp[r + 1][j].first < 2) {
+							cost += dp[r + 1][j].second
+							h = 1 + dp[r + 1][j].first
+						}
+					}
+					r + 1 >= size -> {
+						if (dp[i][r - 1].first < 2) {
+							cost += dp[i][r - 1].second
+							h = 1 + dp[i][r - 1].first
+						}
+					}
+					else -> {
+						if (abs(dp[i][r - 1].first - dp[r + 1][j].first) < 2) {
+							cost += dp[i][r - 1].second + dp[r + 1][j].second
+							h = 1 + max(dp[i][r - 1].first, dp[r + 1][j].first)
+						}
 					}
 				}
+
 				if (cost < minCost) {
 					minCost = cost
-					minR = r
+					minH = h
 				}
 			}
-			dp[i][j] = when {
-				minR - 1 < 0 -> 1 + dp[minR + 1][j].first to minCost
-				minR + 1 >= size -> 1 + dp[i][minR - 1].first to minCost
-				else -> (1 + min(dp[i][minR - 1].first, dp[minR + 1][j].first)) to minCost
-			}
+
+			dp[i][j] = minH to minCost
 		}
 	}
 	return dp[0][size - 1].second
