5dbc70b10ccdad37814a13e8cd9afa60cba0797f
yx-z <yx-z@outlook.com>
2018-03-12 21:38:39 -0500

    largest prod

--- src/dp/LargestProd.kt
+++ src/dp/LargestProd.kt
@@ -8,7 +8,6 @@ import max
 // find the largest product in any contiguous subarray.
 // note that two negative numbers produce a positive result, ex. (-2) * (-4) = 8
 fun main(args: Array<String>) {
-	println(floatArrayOf(1F, 2F, 3F).largestProd())
 	val arr = floatArrayOf(-6F, 12F, -7F, 0F, 14F, -7F, 5F)
 	println(arr.largestProd())
 }
@@ -27,7 +26,26 @@ fun FloatArray.largestProd(): Float {
 	}
 
 	// no 0s in the array
-	var max = this.reduce { acc, fl -> acc * fl }
-	return max
-}
+	var total = this.reduce { acc, fl -> acc * fl }
+	if (total < 0) {
+		var leftMax = total
+		var leftIdx = 0
+		while (leftIdx < size && this[leftIdx] > 0) {
+			leftMax /= this[leftIdx]
+			leftIdx++
+		}
+		// first negative number from left to right
+		leftMax /= this[leftIdx]
 
+		var rightMax = total
+		var rightIdx = size - 1
+		while (rightIdx >= 0 && this[rightIdx] > 0) {
+			rightMax /= this[rightIdx]
+			rightIdx--
+		}
+		// first negative number from right to left
+		rightMax /= this[rightIdx]
+		return max(leftMax, rightMax)
+	}
+	return total
+}
