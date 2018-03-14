52360f1e7496c22df19dba384eb163ce97ce89b3
Yx-Z <yx-z@outlook.com>
2018-03-14 17:29:33 -0500

    fix lconvexs bug

--- src/dp/LConvexS.kt
+++ src/dp/LConvexS.kt
@@ -1,10 +1,12 @@
 package dp
 
+import set
+
 // longest convex subsequence
 // a convex sequence satisfies a[i] - a[i - 1] > a[i - 1] - a[i - 2]
 fun main(args: Array<String>) {
-	val arr = intArrayOf(1, 4, 7, 8)
-	println(lcs(arr))
+	val arr = intArrayOf(1, 4, 8, 13, 14, 18, 19)
+	println(lcs(arr)) // [1, 4, 8, 13, 19] -> 5
 }
 
 fun lcs(arr: IntArray): Int {
@@ -17,13 +19,16 @@ fun lcs(arr: IntArray): Int {
 
 	// dp[i][j] = length of lcs with first two elements arr[i] and arr[j], i < j
 	// dp[i][j] = 1 + max(dp[j][k]) : k in j + 1 until len && arr[k] - arr[j] > arr[j] - arr[i]
-	val dp = Array(len) { IntArray(len) { 2 } }
+	val dp = Array(len) { IntArray(len) }
+	for (i in 0 until len) {
+		dp[i, len - 1] = 2
+	}
 
 	// keep track of max(dp[i][j])
 	var max = 0
 
 	for (i in len - 2 downTo 0) {
-		for (j in i + 1 until len) {
+		for (j in i + 1 until len - 1) {
 			for (k in j + 1 until len) {
 				if (arr[k] - arr[j] > arr[j] - arr[i]) {
 					dp[i][j] = maxOf(dp[i][j], 1 + dp[j][k])
