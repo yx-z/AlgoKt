654b643480df7704d0446e676bf3380a793dc502
yx-z <yx-z@outlook.com>
2018-03-13 19:26:55 -0500

    fixing bugs

--- src/dp/ConstructAVLT.kt
+++ src/dp/ConstructAVLT.kt
@@ -1,63 +1,16 @@
 package dp
 
 import get
-import max
-import kotlin.math.abs
+import set
 
 // similar to 'ConstructBST'
 // construct a balanced BST (AVL Tree) that must satisfy |h(left) - h(right)| < 2
 fun main(args: Array<String>) {
-	val freq = intArrayOf(5)
+	val freq = intArrayOf(1, 1, 1, 1, 10, 1, 1, 100, 1, 1)
 	println(freq.avltCost())
 }
 
+
 fun IntArray.avltCost(): Int {
-	// dp[i][j] = (height in optical avlt, optimal cost for this[i..j])
-	// dp[i][j] = (0, 0), if i > j
-	//          = (1 + max(dp[i, r - 1].first, dp[r + 1, j].first),
-	//            sum(this[i..j] + dp[i, r - 1].second + dp[r + 1, j].second), o/w
-	//            , where r satisfies i <= r <= j, |dp[i, r - 1].first - dp[r + 1, j].first| < 2
-	//            , and minimizes dp[i, r - 1].second + dp[r + 1, j].second
-	val dp = Array(size) { Array(size) { 0 to 0 } }
-	for (j in 0 until size) {
-		for (i in j downTo 0) {
-			var minCost = Int.MAX_VALUE
-			var minH = 0
-			for (r in i..j) {
-				var cost = this[i..j].sum()
-				var h = 0
-				when {
-					r - 1 < 0 && r + 1 >= size -> {
-						// do nothing
-					}
-					r - 1 < 0 -> {
-						if (dp[r + 1][j].first < 2) {
-							cost += dp[r + 1][j].second
-							h = 1 + dp[r + 1][j].first
-						}
-					}
-					r + 1 >= size -> {
-						if (dp[i][r - 1].first < 2) {
-							cost += dp[i][r - 1].second
-							h = 1 + dp[i][r - 1].first
-						}
-					}
-					else -> {
-						if (abs(dp[i][r - 1].first - dp[r + 1][j].first) < 2) {
-							cost += dp[i][r - 1].second + dp[r + 1][j].second
-							h = 1 + max(dp[i][r - 1].first, dp[r + 1][j].first)
-						}
-					}
-				}
-
-				if (cost < minCost) {
-					minCost = cost
-					minH = h
-				}
-			}
-
-			dp[i][j] = minH to minCost
-		}
-	}
-	return dp[0][size - 1].second
+	TODO()
 }
--- src/dp/LargestPattern.kt
+++ src/dp/LargestPattern.kt
@@ -9,75 +9,80 @@ import max
 // the copies of a pattern may overlap but cannot coincide
 // report its size
 fun main(args: Array<String>) {
-	val bitmap = arrayOf(
-			intArrayOf(0, 1, 0),
-			intArrayOf(1, 0, 1),
-			intArrayOf(0, 1, 0))
+    val bitmap = arrayOf(
+            intArrayOf(0, 1, 0, 0, 0),
+            intArrayOf(1, 0, 1, 0, 0),
+            intArrayOf(0, 1, 0, 1, 0),
+            intArrayOf(0, 0, 1, 0, 1),
+            intArrayOf(0, 0, 0, 1, 0))
 
-	println(bitmap.largestPattern())
+    println(bitmap.largestPattern())
+	println(bitmap.largestPatternNoPrime())
+}
+
+fun Array<IntArray>.largestPatternNoPrime(): Int {
+	TODO()
 }
 
 fun Array<IntArray>.largestPattern(): Int {
-	val rows = size
-	val cols = this[0].size
+    val rows = size
+    val cols = this[0].size
 
-	val primes = Array(rows) { IntArray(cols) }
-	val primesFlat = primes(rows * cols)
-	for (row in 0 until rows) {
-		for (col in 0 until cols) {
-			primes[row, col] = primesFlat[cols * row + col]
-		}
-	}
+    val primes = Array(rows) { IntArray(cols) }
+    val primesFlat = primes(rows * cols)
+    for (row in 0 until rows) {
+        for (col in 0 until cols) {
+            primes[row, col] = primesFlat[cols * row + col]
+        }
+    }
 
-	val dp = Array(rows) { Array(cols) { Array(rows) { IntArray(cols) { 1 } } } }
-	val dpFlat = Array(rows * rows * cols * cols) { 0 to (0 to (0 to (0 to 1))) }
-	var dpFlatIdx = 0
-	for (i in 0 until rows) {
-		for (j in 0 until cols) {
-			for (p in i until rows) {
-				for (q in j until cols) {
-					dp[i, j, p, q] = if (this[p, q] == 1) {
-						primes[p - i, q - j]
-					} else {
-						1
-					}
+    val dp = Array(rows) { Array(cols) { Array(rows) { IntArray(cols) { 1 } } } }
+    val dpFlat = Array(rows * rows * cols * cols) { 0 to (0 to (0 to (0 to 1))) }
+    var dpFlatIdx = 0
+    for (i in 0 until rows) {
+        for (j in 0 until cols) {
+            for (p in i until rows) {
+                for (q in j until cols) {
+                    dp[i, j, p, q] = if (this[p, q] == 1) {
+                        primes[p - i, q - j]
+                    } else {
+                        1
+                    }
 
-					if (p - 1 >= 0 && q - 1 >= 0) {
-						dp[i, j, p, q] *= dp[i, j, p - 1, q] * dp[i, j, p, q - 1] / dp[i, j, p - 1, q - 1]
-					} else if (p - 1 >= 0) {
-						dp[i, j, p, q] *= dp[i, j, p - 1, q]
-					} else if (q - 1 >= 0) {
-						dp[i, j, p, q] *= dp[i, j, p, q - 1]
-					}
+                    if (p - 1 >= 0 && q - 1 >= 0) {
+                        dp[i, j, p, q] *= dp[i, j, p - 1, q] * dp[i, j, p, q - 1] / dp[i, j, p - 1, q - 1]
+                    } else if (p - 1 >= 0) {
+                        dp[i, j, p, q] *= dp[i, j, p - 1, q]
+                    } else if (q - 1 >= 0) {
+                        dp[i, j, p, q] *= dp[i, j, p, q - 1]
+                    }
 
-					dpFlat[dpFlatIdx] = i to (j to (p to (q to dp[i, j, p, q])))
-					dpFlatIdx++
-				}
-			}
-		}
-	}
+                    dpFlat[dpFlatIdx] = i to (j to (p to (q to dp[i, j, p, q])))
+                    dpFlatIdx++
+                }
+            }
+        }
+    }
 
-	dpFlat.sortBy { it.second.second.second.second }
-	var maxSize = Int.MIN_VALUE
-	for (i in 1 until dpFlat.size) {
-		if (dpFlat[i].second.second.second.second == dpFlat[i - 1].second.second.second.second) {
-			maxSize = max(maxSize, (dpFlat[i].second.second.first - dpFlat[i].first + 1) * (dpFlat[i].second.second.second.first - dpFlat[i].second.first + 1))
-		}
-	}
+    dpFlat.sortBy { it.second.second.second.second }
+    var maxSize = Int.MIN_VALUE
+    (1 until dpFlat.size).asSequence()
+            .filter { dpFlat[it].second.second.second.second == dpFlat[it - 1].second.second.second.second }
+            .forEach { maxSize = max(maxSize, (dpFlat[it].second.second.first - dpFlat[it].first + 1) * (dpFlat[it].second.second.second.first - dpFlat[it].second.first + 1)) }
 
-	return maxSize
+    return maxSize
 }
 
 // return the first `n` prime numbers
 fun primes(n: Int): IntArray {
-	val ans = IntArray(n)
-	var num = 2
-	for (i in 0 until n) {
-		while (!num.isPrime()) {
-			num++
-		}
-		ans[i] = num
-		num++
-	}
-	return ans
+    val ans = IntArray(n)
+    var num = 2
+    for (i in 0 until n) {
+        while (!num.isPrime()) {
+            num++
+        }
+        ans[i] = num
+        num++
+    }
+    return ans
 }
