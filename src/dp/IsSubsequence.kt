cfed1fd13b63d2303c39bca54f659541f0e73a0b
Yx-Z <yx-z@outlook.com>
2018-03-17 16:11:08 -0500

    longest subsequence of Y that is NOT a supersequence ofe X

--- src/dp/IsSubsequence.kt
+++ src/dp/IsSubsequence.kt
@@ -4,10 +4,9 @@ import get
 import set
 
 // given X[1..k] and Y[1..n] : k <= n
-// determine if X is a subsequence of Y
 
+// 1. determine if X is a subsequence of Y
 // ex. PPAP is a subsequence of PenPineappleApplePie
-
 infix fun IntArray.isSubsequenceOf(Y: IntArray): Boolean {
 	val X = this
 	val k = X.size
@@ -19,11 +18,9 @@ infix fun IntArray.isSubsequenceOf(Y: IntArray): Boolean {
 	// space complexity: O(k * n)
 
 	// base case:
-	// isSub(i, j) = true if i = 0 and j = 0
-	//             = false if i > j || i !in 0..k || j !in 0..n
-	//             = true if i = 0 and j in 1..n
-	dp[0, 0] = true
-	for (j in 1..n) {
+	// isSub(i, j) = true if i = 0 and j in 0..n
+	//             = false if i > j >= 0 || i !in 0..k || j !in 0..n
+	for (j in 0..n) {
 		dp[0, j] = true
 	}
 
@@ -49,6 +46,54 @@ infix fun IntArray.isSubsequenceOf(Y: IntArray): Boolean {
 	return dp[k, n]
 }
 
+// 2. determine the minimum number of elements in Y : after removing those elements
+//    , X is no longer a subsequence of Y
+//    , you can also find the longest subsequence of Y that is not a supersequence of X
+// let me find the length of such subsequence = l
+// and we can know the minimum number of elements to be removed as n - l
+fun IntArray.subseqNotSuperseq(X: IntArray): Int {
+	val Y = this
+	val k = X.size
+	val n = Y.size
+
+	// sns(i, j) = len of subseq of Y[1..j] which is not a supersequnce of X[1..i]
+	// memoization structure: 2d array dp[0..k, 0..n] : dp[i, j] = sns(i, j)
+	val dp = Array(k + 1) { IntArray(n + 1) }
+	// space complexity: O(n * k)
+
+	// base case:
+	// sns(i, 0) = 0
+	// sns(0, j) = j
+	for (j in 0..n) {
+		dp[0, j] = j
+	}
+	// sns(i, j) = j if j < i
+	for (i in 0..k) {
+		for (j in 0 until i) {
+			dp[i, j] = j
+		}
+	}
+
+	// recursive case:
+	// sns(i, j) = sns(i, j - 1) if X[i] = Y[j]
+	//           = 1 + sns(i, j - 1) if X[i] != Y[j]
+	// dependency: sns(i, j) depends on sns(i, j - 1) that is entry to the left
+	// evaluation order: outer loop for i from 1 to k (top down)
+	for (i in 1..k) {
+		// inner loop for j from i to n (left to right)
+		for (j in i..n) {
+			dp[i, j] = dp[i, j - 1]
+			if (X[i - 1] != Y[j - 1]) {
+				dp[i, j] += 1
+			}
+		}
+	}
+	// time complexity: O(n * k)
+
+	// we want sns(k, n)
+	return dp[k, n]
+}
+
 fun main(args: Array<String>) {
 	val X = intArrayOf(3, 5, 1, 2, 6)
 	val Ys = arrayOf(
@@ -58,4 +103,7 @@ fun main(args: Array<String>) {
 	)
 
 	Ys.forEach { println(X isSubsequenceOf it) }
+
+	// output should be 4, 6, 6
+	Ys.forEach { println(it.subseqNotSuperseq(X)) }
 }
