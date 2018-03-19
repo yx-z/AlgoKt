00576f88e53dac9bb2ece550f9be9200793bdd2a
yx-z <yx-z@outlook.com>
2018-03-19 13:32:10 -0500

    setup MaxQ

---
+++ src/dp/MaxQ.kt
@@ -0,0 +1,12 @@
+package dp
+
+import toOneArray
+
+// given a method quality: T[1..k] -> Int that taking O(k) time and a string
+// S[1..n] find the max sum of qualities of each contiguous substring of S
+fun main(args: Array<String>) {
+	val s = "aefdac".toCharArray().toOneArray()
+
+}
+
+
