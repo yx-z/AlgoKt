c393d70d05da60d92d5205ed8e6e8deea7b8d782
yx-z <yx-z@outlook.com>
2018-03-19 13:00:00 -0500

    metalpal complete

--- src/dp/MetaPal.kt
+++ src/dp/MetaPal.kt
@@ -17,6 +17,7 @@ fun main(args: Array<String>) {
 // given S[1..n], find the minimum length of the sequence obtained by metapalindrome
 fun OneArray<Char>.metaPal(): Int {
 	val S = this
+//	S.prettyPrintln()
 	val n = S.size
 
 	// isPal[i, j]: if S[i..j] is palindromic
@@ -26,26 +27,43 @@ fun OneArray<Char>.metaPal(): Int {
 	val isPal = isPal()
 
 	// dp(i): min len of metapal of S[i..n - i + 1]
-	val m = (n + 1) / 2
-	// memoization structure: 1d array dp[1..m : dp[i] = dp(i)
-	val dp = OneArray(m) { 0 }
+	val lM = (n + 1) / 2
+	val rM = n - lM + 1
+	// memoization structure: 1d array dp[1..lM] : dp[i] = dp(i)
+	val dp = OneArray(lM) { 0 }
 	// space complexity: O(n)
 
 	// base case:
-	// dp(m) = 1 if S[m..n - m + 1] is palindromic (which is always the case for odd n)
-	//       = 0 o/w
-	dp[m] = if (isPal[m, n - m + 1]) 1 else 0
+	// dp(lM) = 1 if S[lM..rM] is palindromic (lM = rM in the case of odd n)
+	//        = 2 o/w
+	dp[lM] = if (isPal[lM, rM]) 1 else 2
 
 	// recursive case:
-	// dp(i) =
+	// dp(i) = 1 if S[i..j] is palindromic
+	//         min{ dp(k + 1) + 2 } : i <= k < lM,
+	//         S[i..k] and S[s..j] are both palindromic
+	//         where lM - i = j - rM and k - i = j - s
 	// dependency: dp(i) depends on dp(k) : k > i, that is entries to the right
-	// evaluation order: outer loop for i from m - 1 down to 1 (right to left)
-	for (i in m downTo 1) {
-
+	// evaluation order: outer loop for i from lM - 1 down to 1 (right to left)
+	for (i in lM - 1 downTo 1) {
+		// lM - i = j - rM -> j = lM - i + rM
+		val j = lM - i + rM
+		dp[i] = if (isPal[i, j]) {
+			1
+		} else {
+			(i until lM)
+					.filter { k ->
+						// k - i = j - s -> s = j - (k - i) = j - k + i
+						val s = j - k + i
+						isPal[i, k] && isPal[s, j]
+					}
+					.map { k -> dp[k + 1] + 2 }
+					.min()!!
+		}
 	}
 	// time complexity: O(n^2)
 
-	dp.prettyPrintln()
+//	dp.prettyPrintln()
 
 	// we want dp(1)
 	return dp[1]
