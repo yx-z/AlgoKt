a03fccf5cc01b491cd8d1ca3817eda81e9109898
Yx-Z <yx-z@outlook.com>
2018-03-18 22:52:27 -0500

    redo SplitInPal

--- src/dp/SplitInPal.kt
+++ src/dp/SplitInPal.kt
@@ -1,25 +1,79 @@
 package dp
 
 import get
+import set
+import min
+import OneArray
+import prettyPrintTable
+import toOneArray
 
-// given a string, determine the smallest number of palindromes that make up the string
+// given a String S[1..n],
+// determine the smallest number of palindromes that make up the string
 fun main(args: Array<String>) {
-	val s = "bubabaab" // bub a baab -> 3
-	println(s.splitInPal())
+	val str = "BUBBASEESABANANA".toCharArray().toList().toOneArray()
+	println(str.splitInPal())
 }
 
-fun String.splitInPal(): Int {
-	// dp[i] = # of palindromes that make up this[i until length]
-	// dp[i] = 1, if i == length - 1
-	//       = 1 + min(dp[j + 1]) where j > i && this[i..j].isPal(), o/w
-	val dp = IntArray(length) { Int.MAX_VALUE }
-	dp[length - 1] = 1
-	for (i in length - 2 downTo 0) {
-		dp[i] = 1 + (dp.filterIndexed { idx, _ -> idx - 1 > i && this[i until idx].isPal() }
-				.min() ?: 0)
+fun OneArray<Char>.splitInPal(): Int {
+	val S = this
+	val n = S.size
+//	S.prettyPrintln(true)
+
+	// first we may preprocess to get isPal[i, j] = isPal(S[i..j]), i <= j
+	val isPal = OneArray(n) { OneArray(n) { false } }
+	// space complexity: O(n^2)
+	// base case:
+	// isPai(i, j) = true if i >= j
+	for (i in 1..n) {
+		for (j in 1..i) {
+			isPal[i, j] = true
+		}
 	}
+	// time complexity: O(n^2)
+	// recursive case:
+	// isPal(i, j) = isPal(i + 1, j - 1) && S[i] == S[j]
+	// dependency: isPal[i, j] depends on isPal[i + 1, j - 1]
+	//             , that is entry to the lower-left
+	// evaluation order: outer loop for i from n down to 1 (bottom up)
+	for (i in n downTo 1) {
+		// inner loop for j from i + 1 to n (left to right)
+		for (j in i + 1..n) {
+			if (i != j) {
+				isPal[i, j] = isPal[i + 1, j - 1] && S[i] == S[j]
+			}
+		}
+	}
+	// time complexity: O(n^2)
+//	isPal.prettyPrintTable(true)
 
-	return dp[0]
+	// dp(i): # of pal that can separate S[1..i]
+	// memoization structure: 1d array dp[1..n] : dp[i] = dp(i)
+	val dp = OneArray(n) { 0 }
+	// space complexity: O(n)
+
+	// base case:
+	// dp(i) = 0 if i !in 1..n
+	dp.assignGetOverflowHandler { 0 }
+	// dp(1) = 1
+	dp[1] = 1
+
+	// recursive case:
+	// dp(i) = min_k{ dp(k - 1) } + 1 where S[k..i] is a palindrome, 1 <= k <= i
+	// dependency: dp[i] depends on dp(j) where j < i
+	//             , that is entries to the left
+	// evaluation order: outer loop for i from 2 to n (top down)
+	for (i in 2..n) {
+		// inner loop for k from 1 until i (left to right)
+		dp[i] = (1..i)
+				.filter { k -> isPal[k, i] }
+				.map { k -> dp[k - 1] }.min()!! + 1
+	}
+	// time complexity: O(n^2)
+
+//	dp.prettyPrintln(true)
+
+	// we want dp(n)
+	return dp[n]
+	// overall space complexity: O(n^2)
+	// overall time complexity: O(n^2)
 }
-
-fun String.isPal() = this == this.reversed()
