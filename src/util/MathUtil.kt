c43fc3772a9ec8149e22b7e76110900472f44dd8
Yx-Z <yx-z@outlook.com>
2018-03-22 10:22:49 -0500

    generics with min/max

--- src/util/MathUtil.kt
+++ src/util/MathUtil.kt
@@ -2,14 +2,8 @@ package util
 
 // multi-parameter version of util.min/util.max
 // ex. util.min(i1, i2, i3, i4), util.max(i1, i2, i3, i4, i5)
-fun min(vararg ints: Int) = ints.min()
-		?: throw NullPointerException("no minimum value")
-
-fun min(vararg doubles: Double) = doubles.min()
-		?: throw NullPointerException("no minimum value")
-
-fun max(vararg ints: Int) = ints.max()
+fun <T : Comparable<T>> max(vararg ts: T) = ts.max()
 		?: throw NullPointerException("no maximum value")
 
-fun max(vararg doubles: Double) = doubles.max()
+fun <T : Comparable<T>> min(vararg ts: T) = ts.min()
 		?: throw NullPointerException("no minimum value")
