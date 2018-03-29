626a80be10e8064cef31792041b3044dc9b23f20
Yx-Z <yx-z@outlook.com>
2018-03-28 19:57:13 -0500

    karatsuba

--- src/recur/Multiplication.kt
+++ src/recur/Multiplication.kt
@@ -2,6 +2,8 @@ package recur
 
 import math.time
 import java.lang.Math.floor
+import java.lang.Math.pow
+import kotlin.math.ceil
 
 // divide and conquer multiplication
 fun mult(x: Int, y: Int): Long {
@@ -18,15 +20,36 @@ fun mult(x: Int, y: Int): Long {
 	return prod
 }
 
+// x * y, each of which are n digit numbers
+fun karastuba(x: Int, y: Int, n: Int): Long {
+	if (n == 1) {
+		return (x * y).toLong()
+	}
+
+	val m = ceil(n / 2.0)
+	val p = pow(10.0, m)
+	val a = floor(x / p)
+	val b = x % p
+	val c = floor(y / p)
+	val d = y % p
+	val e = karastuba(a.toInt(), c.toInt(), m.toInt())
+	val f = karastuba(b.toInt(), d.toInt(), m.toInt())
+	val g = karastuba(b.toInt(), c.toInt(), m.toInt())
+	val h = karastuba(a.toInt(), d.toInt(), m.toInt())
+	return (p * p * e + p * (g + h) + f).toLong()
+}
+
 fun main(args: Array<String>) {
 	val M1 = 12345
 	val M2 = 54321
 
 	val direct = time({ M1 * M2 } * 100)
 	val indirect = time({ mult(M1, M2) } * 100)
+	val karastuba = time({ karastuba(M1, M2, 5) * 100 })
 
 	println("direct method takes $direct ms")
 	println("indirect method takes $indirect ms")
+	println("karastuba's method takes $karastuba ms")
 }
 
 // invoke the method `max` times
