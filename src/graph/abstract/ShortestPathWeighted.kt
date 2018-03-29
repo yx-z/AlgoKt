44352a7ccae1af13b08af5be5504af801728d148
Yx-Z <yx-z@outlook.com>
2018-03-28 22:55:28 -0500

    bellman ford

--- src/graph/abstract/ShortestPathWeighted.kt
+++ src/graph/abstract/ShortestPathWeighted.kt
@@ -1,6 +1,7 @@
 package graph.abstract
 
 import util.Tuple2
+import util.min
 import util.tu
 import java.util.*
 import kotlin.collections.HashMap
@@ -36,6 +37,54 @@ fun <V> WeightedGraph<V, Int>.dijkstra(s: Vertex<V>)
 	return dist tu parent
 }
 
+// if we use a queue instead of minHeap in the above classic Dijkstra's Algorithm,
+// it will be called Shimbel's or Bellman-Ford's Alogrithm
+
+// but we can also do a DP version of Shimbel's algorithm
+fun <V> WeightedGraph<V, Int>.shimbelDP(s: Vertex<V>, t: Vertex<V>): Int {
+	val V = vertices.size
+	val INF = Int.MAX_VALUE / 2
+
+	// dp(i, v): shortest distance from s to v consisting of at most i edges
+	// memoization structure: array of maps dp[1 until V, vertices ] : dp[i, v] = dp(i, v)
+	val dp = Array(V) { HashMap<Vertex<V>, Int>() }
+
+	// base case:
+	// dp(0, s) = 0
+	// dp(0, v) = inf for all v != s
+	vertices.forEach { vertex ->
+		dp[0][vertex] = INF
+	}
+	dp[0][s] = 0
+	// space complexity: O(V^2)
+
+	// recursive case:
+	// dp(i, v) = min { dp(i - 1, v), min { dp(i - 1, u) + w(u -> v) : u -> v in E } }
+	// dependency: dp(i, v) depends on dp(i - 1, u)
+	// evaluation order: outer loop for i from 1 until V
+	for (i in 1 until V) {
+		// inner loop for every vertex v
+		vertices.forEach { v ->
+			dp[i][v] = dp[i - 1][v]!!
+			weightedEdges
+					.filter { (s, e) -> e === v } // get all edges to v
+					.forEach { (u, _, _, d) ->
+						dp[i][v] = min(dp[i][v]!!, dp[i - 1][u]!! + d!!)
+					}
+		}
+	}
+	// time complexity: O(VE)
+
+	// we want dp(V - 1, t)
+	return dp[V - 1][t]!!
+}
+
+// we can optimize the above algorithm by observing that we only need dp[i - 1]
+// to compute dp[i]
+// so we don't need dp[1 until V] instead, a single map containing previous
+// results is enough
+
+
 fun main(args: Array<String>) {
 	val vertices = (1..5).map { ComparableVertex(it) }
 	val edges = setOf(
@@ -46,5 +95,6 @@ fun main(args: Array<String>) {
 			WeightedEdge(vertices[2], vertices[4], true, 3),
 			WeightedEdge(vertices[3], vertices[4], true, 1))
 	val graph = WeightedGraph(vertices, edges)
-	println(graph.dijkstra(vertices[0]))
+	println(graph.dijkstra(vertices[0]).first[vertices[4]])
+	println(graph.shimbelDP(vertices[0], vertices[4]))
 }
