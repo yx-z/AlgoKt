c40da7967ec3a1e45728135e3de3647495c00b93
Yx-Z <yx-z@outlook.com>
2018-03-29 22:38:59 -0500

    add time complexity analysisΩ

--- src/recur/PreInToBinTree.kt
+++ src/recur/PreInToBinTree.kt
@@ -8,9 +8,11 @@ import util.prettyPrintTree
 import kotlin.collections.set
 
 // given two arrays Pre[1..n] and In[1..n] representing the preorder and inorder
-// sequences for a binary tree, reconstruct such tree or return -1 for exceptions
+// sequences for a binary tree, reconstruct such tree or throw exceptions
 
-// first we can preprocess in O(n) time : In[PreToIn[i]] = Pre[i]
+// first we can preprocess in O(n) time : In[PreToIn[i]] = Pre[i], i.e.
+// PreToIn[i] gives the index in the inorder array In that represents the same
+// element in the preorder array Pre
 fun <T> preprocess(Pre: OneArray<T>, In: OneArray<T>): OneArray<Int> {
 	val n = Pre.size
 	val dict = HashMap<T, Int>()
@@ -19,7 +21,7 @@ fun <T> preprocess(Pre: OneArray<T>, In: OneArray<T>): OneArray<Int> {
 	}
 	val PreToIn = OneArray(n) { 0 }
 	for (i in 1..n) {
-		PreToIn[i] = dict[Pre[i]]!!
+		PreToIn[i] = dict[Pre[i]]!! // <- throws exceptions if we cannot find Pre[i] in In[i]
 	}
 	return PreToIn
 }
@@ -41,6 +43,7 @@ fun <T> binTreeFrom(Pre: OneArray<T>,
 	root.right = binTreeFrom(Pre[r + 1..n], In[r + 1..n])
 	return root
 }
+// time complexity: O(n log n)
 
 fun main(args: Array<String>) {
 	val Pre = oneArrayOf('a', 'b', 'd', 'e', 'c', 'f')
