c19068a5407887693429679be462174919eec8e8
yx-z <yx-z@outlook.com>
2018-04-11 13:10:49 -0500

    hanoi variation

--- src/recur/LibertyTowers.kt
+++ src/recur/LibertyTowers.kt
@@ -6,13 +6,10 @@ package recur
 // but say it's currently at peg i, you can only move it to peg i - 1 or i + 1
 // and as Tower of Hanoi, large disks cannot be placed on smaller disks
 
-// you don't need to find the most efficient strategy
+// you don't need to find the most optimal strategy
 
-fun libertyTowers3(n: Int): Int {
-	return libertyTowers3Recur(n, 1, 3)
-}
-
-private fun libertyTowers3Recur(n: Int, start: Int, end: Int): Int {
+// 1. solve a special case for k = 3
+fun libertyTowers3(n: Int, start: Int = 1, end: Int = 3): Int {
 	if (n == 0 || start == end) {
 		return 0
 	}
@@ -21,59 +18,59 @@ private fun libertyTowers3Recur(n: Int, start: Int, end: Int): Int {
 	when (start) {
 		1 -> {
 			if (end == 2) {
-				count += libertyTowers3Recur(n - 1, 1, 3)
+				count += libertyTowers3(n - 1, 1, 3)
 
 				move(n, 1, 2)
 				count++
 
-				count += libertyTowers3Recur(n - 1, 3, 2)
+				count += libertyTowers3(n - 1, 3, 2)
 			} else { // end == 3
-				count += libertyTowers3Recur(n - 1, 1, 3)
+				count += libertyTowers3(n - 1, 1, 3)
 
 				move(n, 1, 2)
 				count++
 
-				count += libertyTowers3Recur(n - 1, 3, 2)
-				count += libertyTowers3Recur(n, 2, 3)
+				count += libertyTowers3(n - 1, 3, 2)
+				count += libertyTowers3(n, 2, 3)
 			}
 		}
 		2 -> {
 			if (end == 1) {
-				count += libertyTowers3Recur(n - 1, 2, 3)
+				count += libertyTowers3(n - 1, 2, 3)
 
 				move(n, 2, 1)
 				count++
 
-				count += libertyTowers3Recur(n - 1, 3, 2)
-				count += libertyTowers3Recur(n - 1, 2, 1)
+				count += libertyTowers3(n - 1, 3, 2)
+				count += libertyTowers3(n - 1, 2, 1)
 			} else { // end == 3
-				count += libertyTowers3Recur(n - 1, 2, 1)
+				count += libertyTowers3(n - 1, 2, 1)
 
 				move(n, 2, 3)
 				count++
 
-				count += libertyTowers3Recur(n - 1, 1, 2)
-				count += libertyTowers3Recur(n - 1, 2, 3)
+				count += libertyTowers3(n - 1, 1, 2)
+				count += libertyTowers3(n - 1, 2, 3)
 			}
 		}
 		3 -> {
 			if (end == 2) {
-				count += libertyTowers3Recur(n - 1, 3, 2)
-				count += libertyTowers3Recur(n - 1, 2, 1)
+				count += libertyTowers3(n - 1, 3, 2)
+				count += libertyTowers3(n - 1, 2, 1)
 
 				move(n, 3, 2)
 				count++
 
-				count += libertyTowers3Recur(n - 1, 1, 2)
+				count += libertyTowers3(n - 1, 1, 2)
 			} else { // end == 1
-				count += libertyTowers3Recur(n - 1, 3, 2)
-				count += libertyTowers3Recur(n - 1, 2, 1)
+				count += libertyTowers3(n - 1, 3, 2)
+				count += libertyTowers3(n - 1, 2, 1)
 
 				move(n, 3, 2)
 				count++
 
-				count += libertyTowers3Recur(n - 1, 1, 2)
-				count += libertyTowers3Recur(n, 2, 1)
+				count += libertyTowers3(n - 1, 1, 2)
+				count += libertyTowers3(n, 2, 1)
 			}
 		}
 	}
