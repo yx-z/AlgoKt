77a6e85e25f52cc0b13353f152178a616c4ccbc0
Yx-Z <yx-z@outlook.com>
2018-04-12 23:17:10 -0500

    cutvertex

--- src/graph/variation/CutVertex.kt
+++ src/graph/variation/CutVertex.kt
@@ -1,7 +1,20 @@
 package graph.variation
 
-import graph.core.*
-import util.*
+import graph.core.Edge
+import graph.core.Graph
+import graph.core.Vertex
+import graph.core.topoSort
+import util.OneArray
+import util.max
+import util.min
+import kotlin.collections.HashMap
+import kotlin.collections.HashSet
+import kotlin.collections.Set
+import kotlin.collections.any
+import kotlin.collections.forEach
+import kotlin.collections.map
+import kotlin.collections.set
+import kotlin.collections.setOf
 
 // cut vertex is a vertex : after deleting it, the graph will be disconnected
 
@@ -37,18 +50,83 @@ fun <V> Graph<V>.dagCutVertex(checkIdentity: Boolean = true): Boolean {
 	return V.indices.any { it in 2 until V.size && dp[it - 1] <= it }
 }
 
-// 2. find a cut vertex in a general graph
+// 2. find all cut vertices in a general directed graph
 //    google articulation point (another name for cut vertex) would help
-fun <V> Graph<V>.cutVertex(checkIdentity: Boolean = true): Boolean {
+fun <V> Graph<V>.cutVertex(checkIdentity: Boolean = true): Set<Vertex<V>> {
 	// so the basic idea is, run a DFS for G, v in V is a cut vertex iff. either
 	// - v is the root of the DFS tree and has at least two children
 	//   OR
-	// - v is NOT the root of the DFS tree and there is no back edge from the
-	//   subtree of v to v's parent
+	// - v is NOT the root of the DFS tree and there is a child w : no back edge
+	//   from the subtree of w to v's parent
 	// reference: https://www.geeksforgeeks.org/articulation-points-or-cut-vertices-in-a-graph/
 
+	val cutVertices = HashSet<Vertex<V>>()
+	val marked = HashMap<Vertex<V>, Boolean>()
+	val parent = HashMap<Vertex<V>, Vertex<V>?>()
+	val disc = HashMap<Vertex<V>, Int>()
+	val low = HashMap<Vertex<V>, Int>()
+	vertices.forEach { v ->
+		marked[v] = false
+		parent[v] = null
+		disc[v] = 0
+		low[v] = 0
+	}
 
-	TODO()
+	var discTime = 0
+	vertices.forEach { v ->
+		if (marked[v]!! == false) {
+			discTime = cutVertexRecur(
+					v,
+					marked,
+					parent,
+					disc,
+					low,
+					cutVertices,
+					discTime,
+					checkIdentity)
+		}
+	}
+
+	return cutVertices
+}
+
+private fun <V> Graph<V>.cutVertexRecur(v: Vertex<V>,
+                                        marked: HashMap<Vertex<V>, Boolean>,
+                                        parent: HashMap<Vertex<V>, Vertex<V>?>,
+                                        disc: HashMap<Vertex<V>, Int>,
+                                        low: HashMap<Vertex<V>, Int>,
+                                        cutVertices: HashSet<Vertex<V>>,
+                                        pDiscTime: Int,
+                                        checkIdentity: Boolean = true): Int {
+	var discTime = pDiscTime
+	var children = 0
+	marked[v] = true
+	disc[v] = discTime
+	low[v] = discTime
+	getEdgesFrom(v, checkIdentity).forEach { (_, w) ->
+		if (marked[w] == false) {
+			children++
+			parent[w] = v
+			discTime = cutVertexRecur(
+					w,
+					marked,
+					parent,
+					disc,
+					low,
+					cutVertices,
+					discTime,
+					checkIdentity)
+			low[w] = min(low[w]!!, low[v]!!)
+			if (parent[v] == null && children > 1) {
+				cutVertices.add(v)
+			}
+
+			if (parent[v] != null && low[w]!! >= disc[v]!!) {
+				cutVertices.add(w)
+			}
+		}
+	}
+	return discTime + 1
 }
 
 fun main(args: Array<String>) {
@@ -60,4 +138,5 @@ fun main(args: Array<String>) {
 			Edge(V[2], V[3], true))
 	val G = Graph(V, E)
 	println(G.dagCutVertex())
+	println(G.cutVertex())
 }
