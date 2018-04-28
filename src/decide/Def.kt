2e838eab5947bd2d062274b9e4413806c7f54506
yx-z <yx-z@outlook.com>
2018-04-28 13:37:05 -0500

    more decision problems

--- src/decide/Def.kt
+++ src/decide/Def.kt
@@ -2,16 +2,20 @@
 
 // naming convention
 typealias Input = String
-typealias Encoding = String
-typealias TM /* Turing Machine */ = (Input) -> Boolean 
+
+typealias TM /* Turing Machine */ = (Input) -> Boolean
+
+// useful definition in some problems, ex. halting problem
+// it is still a valid turing machine since we can transform the first parameter
+// TM, into a form of its proper encoding, which is still a string input
+typealias TM2 /* Turing Machine w/ 2 params */ = (TM, Input) -> Boolean
 
 // loop forever
 fun loop(): Boolean {
     while(true)
-    // not gonna be here
-    return true
+    // NOT gonna be here, i.e. will NOT halt at all
+    return halt()
 }
 
-// halt (don't care true or false)
-// have fun with some randomness
-fun halt() = Math.random() > 0.5
+// halt (don't care about true or false)
+fun halt() = Math.random() >= 0.5 // have fun with some randomness
